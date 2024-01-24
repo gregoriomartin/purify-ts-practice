2d9fe731577c8cfd6fe50be04304fa74aa89722d
Gregorio Martin <gregorio.martin92@gmail.com>
2024-01-24 12:19:08 -0300

    More examples

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Maybe, Just, Nothing } from 'purify-ts/Maybe'
+import { Either, Left, Right } from 'purify-ts/Either'
 
+// Maybe
 const getData = (params: { first: string; second?: string }) => Maybe.fromNullable(params.second)
   .chain(s => Just(`${params.first} ${s}`))
   .orDefault(params.first);
@@ -17,6 +19,31 @@ const fromPredicateExample = Maybe.fromPredicate((x: number) => x > 10, 15);
 const catMaybesExample = Maybe.catMaybes([Just(1), Nothing, Just(3)]);
 const mapMaybeExample = Maybe.mapMaybe((x: number) => x > 1 ? Just(x) : Nothing, [1, 2, 3]);
 
+// Either
+const parseJSON = (json: string): Either<Error, string> => {
+  try {
+    return Right(JSON.stringify(JSON.parse(json)));
+  } catch (error) {
+    console.log(error);
+    return Left(Error('Invalid JSON'));
+  }
+};
+
+const right = parseJSON('{"validJson": true}');
+const left = parseJSON('invalid json');
+
+const lefEncase = Either.encase(() => { throw Error('Just Failed')});
+const rightEncase = Either.encase(() => JSON.stringify(JSON.parse('{"right": true}')));
+
+const simpleRightExample = Right('Right Example').map(value => value.toUpperCase());
+const simpleLeftExample = Left('Left Example').mapLeft(error => error.toLowerCase());
+const chainExample = Right(5).chain(value => Right(value * 2));
+const chainLeftExample = Left('Error').chainLeft(err => Left(err + ' Chain'));
+const apExample = Right((x: number) => x * 2).ap(Right(x => x));
+//Returns `this` if it's a `Left`, otherwise it returns the result of applying the function argument to `this` and wrapping it in a `Right`.
+const extendExample = Left('Wrong Value').extend(e => e.isRight()); 
+
+
 function App() {
   return (
     <div className="App">
@@ -30,6 +57,17 @@ function App() {
         <p>fromPredicate Example: {fromPredicateExample.orDefault(15)}</p>
         <p>catMaybes Example: {catMaybesExample.join(', ')}</p>
         <p>mapMaybe Example: {mapMaybeExample.join(', ')}</p>
+
+        <p>Right Example: {right.orDefault('Error')}</p>
+        <p>Left Example: {left.orDefault('Error')}</p>
+        <p>Right Encase Example: {rightEncase.isRight() == true ? String(rightEncase.extract()) : 'Left Encase' }</p>
+        <p>Left Encase Example: {lefEncase.leftOrDefault(Error('Fail')).message}</p>
+        <p>Simple Right Example: {String(simpleRightExample)}</p>
+        <p>Simple Left Example: {String(simpleLeftExample)}</p>
+        <p>Chain example: {String(chainExample)}</p>
+        <p>Chain left example: {String(chainLeftExample)}</p>
+        <p>Ap example: {String(apExample)}</p>
+        <p>extend example: {String(extendExample)}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
